feb5ad5f352e781e03aa6775a09cb7913bf535fb
kunalJa <kunaljain@utexas.edu>
2018-07-15 05:02:40 -0500

    Chanfed

--- contracts/TrustMe.sol
+++ contracts/TrustMe.sol
@@ -3,41 +3,27 @@ pragma solidity 0.4.24;
 contract TrustMe {
     address owner;
 
-    struct fundraiser {
-        uint256 unique_id;
-        uint256 money;
-    }
-
-    mapping(address => fundraiser[]) charities;
+    mapping(address => uint256) charities;
 
     constructor() public {
       owner = msg.sender;
    }
 
-   function initializeFundraiser(address organization, uint256 id) public {
-       charities[organization].push(fundraiser(id, 0));
+   function initializeFundraiser(address organization) public {
+       charities[organization] = 0;
    }
 
-   function donate(uint256 ammount, address organization, uint256 id) public payable {
+   function donate(uint256 ammount, address organization) public payable {
         require(
            ammount > 0,
            "Invalid donation ammount"
         );
 
-        charities[organization][id].money += msg.value;
+        charities[organization] += msg.value;
    }
 
-   function withdraw(address organization, uint256 id) public returns (bool) {
-       uint amount = charities[organization][id].money;
-       if (amount > 0) {
-           charities[organization][id].money = 0;
-
-           if (!organization.send(amount)) {
-               charities[organization][id].money = amount;
-               return false;
-           }
-       }
-       return true;
+   function withdraw(address organization) public {
+       organization.transfer(charities[organization]);
    }
 
 }
